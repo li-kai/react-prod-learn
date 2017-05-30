e28d3734af060154af315eb665fdfc57e8c89180
li-kai <falconets25601@gmail.com>
2017-05-30 14:19:19 +0800

    Add favicons

--- src/server.js
+++ src/server.js
@@ -1,21 +1,39 @@
+// Node utilities
 import path from 'path';
 import dotenv from 'dotenv';
-import Express from 'express';
+
+// Express server dependencies
+import express from 'express';
+import favicon from 'serve-favicon';
+
+// React server dependencies
 import React from 'react';
 import { renderToString, renderToStaticMarkup } from 'react-dom/server';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
+// Configuration paths
 import PATHS from '../config/paths';
 
+// React components
 import rootReducer from './reducers';
 import App from './App';
 import HTMLDocument from './components/HTMLDocument';
 
+// Import environment variables
 dotenv.config({ path: path.join(PATHS.root, '.env.development') });
+
+// If you use Docker, Vagrant or Cloud9, set
+// host: options.host || '0.0.0.0';
+//
+// 0.0.0.0 is available to all network devices
+// unlike default `localhost`.
+const PORT = parseInt(process.env.PORT, 10) || 8080;
+const HOST = process.env.HOST || 'localhost';
+const PROTOCOL = process.env.HTTPS === 'true' ? 'https' : 'http';
 const IS_DEV_ENV = process.env.NODE_ENV === 'development';
 
-const server = Express();
+const server = express();
 
 let webpackAssets;
 if (IS_DEV_ENV) { /* eslint-disable global-require, import/no-extraneous-dependencies */
@@ -60,13 +78,19 @@ function handleRender(req, res) {
   res.send(`<!DOCTYPE html>${html}`);
 }
 
+// Serves favicons
+server.use(favicon(path.join(PATHS.public, 'favicons', 'favicon.ico')));
+
 // This is fired every time the server side receives a request
 server.use(handleRender);
 
 // Finally, start the express server
-const port = process.env.PORT;
-server.listen(port, (error) => {
+server.listen(PORT, (error) => {
   if (!error) {
-    console.log(`Server started at port ${port}`);
+    console.log(`Server started at port ${PORT}`);
+    if (IS_DEV_ENV) {
+      const openBrowser = require('react-dev-utils/openBrowser');
+      openBrowser(`${PROTOCOL}://${HOST}:${PORT}/`);
+    }
   }
 });
